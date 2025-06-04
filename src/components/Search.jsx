53f4d66f98e7c34ad0b1190527b2577423d07d54
Esmail Nbaheen <esmailmay44@gmail.com>
2025-06-04 16:25:28 +0300

    Hero section

---
+++ src/components/Search.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div className="text-white text-3xl">
+        <img src="search.svg" alt="Search Icon" />
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <h1 className="text-white">{searchTerm}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
